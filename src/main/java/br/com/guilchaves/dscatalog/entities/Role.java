7bb69a35cb8abbdc2325a7ecc0eb5d35c3715be3
guilherme <chaves.guilherme@proton.me>
2024-01-10 14:58:12 -0300

    feat: add role and user entities with mapping

---
+++ src/main/java/br/com/guilchaves/dscatalog/entities/Role.java
@@ -0,0 +1,60 @@
+package br.com.guilchaves.dscatalog.entities;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serial;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements GrantedAuthority {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String authority;
+
+    public Role(){
+
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        return Objects.equals(authority, role.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return authority != null ? authority.hashCode() : 0;
+    }
+}
