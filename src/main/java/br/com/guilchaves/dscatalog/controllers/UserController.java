0235b98df1f9c295d5c87bcc87c7895d57997a41
guilherme <chaves.guilherme@proton.me>
2024-01-10 17:41:50 -0300

    feat: add route role authorization

--- src/main/java/br/com/guilchaves/dscatalog/controllers/UserController.java
+++ src/main/java/br/com/guilchaves/dscatalog/controllers/UserController.java
@@ -2,12 +2,12 @@ package br.com.guilchaves.dscatalog.controllers;
 
 import br.com.guilchaves.dscatalog.dto.UserDTO;
 import br.com.guilchaves.dscatalog.dto.UserInsertDTO;
-import br.com.guilchaves.dscatalog.entities.User;
 import br.com.guilchaves.dscatalog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -21,18 +21,21 @@ public class UserController {
     private UserService service;
 
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @GetMapping
     public ResponseEntity<Page<UserDTO>> findAll(Pageable pageable) {
         Page<UserDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok(dto);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
         UserDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto) {
         UserDTO newDto = service.insert(dto);
@@ -44,12 +47,14 @@ public class UserController {
         return ResponseEntity.created(uri).body(newDto);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
