5c13d86be9bbb56e6ccbd4c142002e28686d6efa
guilherme <chaves.guilherme@proton.me>
2024-01-16 14:27:18 -0300

    feat: return list not page for category findall

--- src/main/java/br/com/guilchaves/dscatalog/services/CategoryService.java
+++ src/main/java/br/com/guilchaves/dscatalog/services/CategoryService.java
@@ -23,9 +23,9 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public Page<CategoryDTO> findAll(Pageable pageable) {
-        Page<Category> result = repository.findAll(pageable);
-        return result.map(CategoryDTO::new);
+    public List<CategoryDTO> findAll() {
+        List<Category> result = repository.findAll();
+        return result.stream().map(CategoryDTO::new).toList();
     }
 
     @Transactional(readOnly = true)
